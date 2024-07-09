8e2921d6ee5c36bfab190e0efc9eedd07a72a8d6
EJ Corpuz <ejan.corp@gmail.com>
2024-07-10 02:08:17 +0400

    update

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const moment = require('moment-timezone');
+
 const app = express();
-const port = 3000;
+const port = 80;
+
 
 app.get('/', (req, res) => {
-  res.json({ vpn: false });
+
+  console.log('GET request received');
+
+  const currentTimeUTC = moment().utc();
+  const philippineTime = currentTimeUTC.tz('Asia/Manila').format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
+
+  res.json({
+    "androidversion": 0,
+    "checkvpn": false,
+    "country": {
+      "country": {
+        "iso_code": "PH"
+      }
+    },
+    "forceUpdate": false,
+    "iosversion": 0,
+    "ip": "119.94.32.185",
+    "systemTime": philippineTime,
+    "vmsversion": "release-Vivaone-R41-10",
+    "voucherRedemption": true
+  })
 });
 
 app.listen(port, () => {
